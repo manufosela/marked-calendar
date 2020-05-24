1e59f2936cc41d2783a7393fa4a9c86d00d46c3e
Manuel Juan Fosela Águila <manufosela@gmail.com>
2020-05-24 11:46:01 +0200

    add terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,22 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   preserveSymlinks: true,
-  input: ['marked-calendar.js'],
+  input: [ 'marked-calendar.js' ],
   output: {
-    file: 'build/marked-calendar.js',
+    file: 'dist/marked-calendar.js',
     format: 'es',
     sourcemap: true
   },
   plugins: [
     resolve(),
-    babel()
+    babel(),
+    terser({
+      output: {
+        comments: false
+      }
+    })
   ]
 };
